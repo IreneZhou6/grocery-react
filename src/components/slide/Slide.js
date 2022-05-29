9f7092ca338f2614269ac70a17db885bf75c2921
Irene Z <2511486093@qq.com>
2022-05-29 19:03:46 +0800

    rem adjustment

--- src/components/slide/Slide.js
+++ src/components/slide/Slide.js
@@ -33,8 +33,8 @@ export default function Slide() {
         <section className='slide'>
             <div className='slide-container'>
                 <div className='slide-content'>
-                    <p>fresh and organic</p>
-                    <h3>upto 50% off</h3>
+                    <p>Fresh And Organic</p>
+                    <h3>Upto 50% Off</h3>
                     <button className='btn'>Shop Now</button>
                 </div>
                 <img src={Bg[slideIndex]} alt='vegetables' />
